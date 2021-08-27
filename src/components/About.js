cdbd7ffb47d364846c7ed980a5faeb3e8635937b
Caleb <cpbeachler@gmail.com>
2021-08-27 14:24:27 -0400

    final touches

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,15 @@ const About = () => {
 
 
     return(
-        <div className='about'>
-            <p className='aboutText'>
-                I am a born learner, proficient in analyzing problems and implementing solutions. I come from a background of demonstrating expertise working within a team environment, in both leading and team member roles. Currently seeking an opportunity to advance my career as well as contribute to your team.
-                Below is a list of my proficiencies and technologies I am experienced in, as well as links to my projects.
-            </p>
-    </div>
+        <div className='aboutAll'>
+            <h3>About</h3>
+            <div className='about'>
+                <p className='aboutText'>
+                    I am a born learner, proficient in analyzing problems and implementing solutions. I come from a background of demonstrating expertise working within a team environment, in both leading and team member roles. Currently seeking an opportunity to advance my career as well as contribute to your team.
+                    Below is a list of my proficiencies and technologies I am experienced in, as well as links to my projects.
+                </p>
+            </div>
+        </div>
     )
 }
 
--- src/components/Sites.js
+++ src/components/Sites.js
@@ -8,19 +8,23 @@ const Sites = () => {
 
     const [focused, setFocused] = useState()
     const [info, setInfo] = useState('Please explore any of my above projects!')
+    const [name, setName] = useState('')
 
     const hover = (site) => {
         switch (site) {
             case 'cranium' :
                 setInfo('Manages the creation and locating of decks of flash cards for users to study (clone inspiration: Brainscape)')
+                setName('CraniumLand')
                 setFocused('cranium')
                 break;
             case 'tradr' :
                 setInfo('Allows users to manage a collection of trading cards that will match users based off of their collections')
+                setName('Tradr')
                 setFocused('tradr')
                 break;
             case 'podemic' :
                 setInfo('Social platform pertaining to podcast reviews and user comments with ratings (clone inspiration: GoodReads)')
+                setName('Podemic')
                 setFocused('podemic')
                 break;
             default :
@@ -30,6 +34,7 @@ const Sites = () => {
     }
     return (
         <div className='siteContent'>
+            <h3>Projects</h3>
             <div className='siteHolder'>
                 <a href='https://podemic.herokuapp.com/' target="_blank" rel="noreferrer" onMouseEnter={() => hover('podemic')}>
                     <img alt='Podemic' src={podemicLogo} className={(focused === 'podemic') ? `logo, focused`: 'logo'}/>
@@ -42,6 +47,9 @@ const Sites = () => {
                 </a>
             </div>
             <div className='siteDescription'>
+                {name &&
+                <h3>{name}</h3>
+                }
                 {info}
             </div>
         </div>
